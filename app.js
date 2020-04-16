e56c7c611d2c1cb8eca08451801df3f9762600f6
SBALDOCK <stephenbaldock78@gmail.com>
2020-04-16 07:57:30 -0700

    javascriptadd

--- app.js
+++ app.js
@@ -5,25 +5,35 @@ var greeting;
 if (hourNow > 18) {
   greeting = 'Ah, I see you dig that evening music!';
 } else if (hourNow > 12) {
-  greeting = 'You dig those lunchtime beats? Dig.';
+  greeting = 'You dig those lunchtime beats? Peachy.';
 } else if (hourNow >= 0) {
-  greeting = 'Give me my coffee and some tunes for my commute!';
+  greeting = 'I see you dig those morning tunes. Nice.';
 } else {
   greeting = 'Dig those funky tunes';
 }
 
 document.write('<h3>' + greeting + '</h3>');
 
-var person = prompt("Please enter your name", "Frank Zappa");
+
+var person = prompt("Please enter your name", "Hinkle McKrinkleberry");
 
 if (person != null) {
   document.getElementById("demo").innerHTML =
   "Hello " + person + "! How are you today?";
 }
 
-var genre = prompt("Please enter your favorite genre of music", "Soul");
-
-if (genre != null) {
-  document.getElementById("genre").innerHTML =
-  "Great taste in tunes you funky beast!";
+function myFunction() {
+  var genre = prompt("What's your favorite genre of music?", "Soul");
+  if (genre!= null) {
+    document.getElementById("genre").innerHTML =
+    "Great taste in tunes you funky beast!";
+  }
 }
+
+
+//  var genre = prompt("Please enter your favorite genre of music", "Soul");
+
+// if (genre != null) {
+  // document.getElementById("genre").innerHTML =
+  // "Great taste in tunes you funky beast!";
+// }
