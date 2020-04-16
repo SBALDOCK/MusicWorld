b6e934943fbc6bacd435050b0eeb2026d6135bf8
SBALDOCK <stephenbaldock78@gmail.com>
2020-04-15 20:38:19 -0700

    anotherjavascript

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 var today = new Date();
-var hourNow = today.getHours();
+var hourNow = today.getHours(); 
 var greeting;
 
 if (hourNow > 18) {
-    greeting - 'I see you like that late night music!';
+  greeting = 'Ah, I see you dig that evening music!';
 } else if (hourNow > 12) {
-    greeting = 'Ah, you dig those work day tunes. Nice!';
-} else if (hourNow > 0) {
-    greeting = 'You clearly enjoy those morning coffee playlists!';
+  greeting = 'You dig those lunchtime beats? Dig.';
+} else if (hourNow >= 0) {
+  greeting = 'Give me my coffee and some tunes for my commute!';
 } else {
-    greeting = 'You should seriously listen to Bill Withers my dudes!';
+  greeting = 'Dig those funky tunes';
 }
 
-// document.write('<h3>' + greeting + '</h3>');
+document.write('<h3>' + greeting + '</h3>');
 
 
 var genre = prompt("Please enter your favorite genre of music", "Soul");
